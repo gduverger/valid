d219218b9baa18453ff1da87a674bb30e2189416
Georges Duverger <georges.duverger@gmail.com>
2009-11-27 17:00:24 -0500

    Add documentation

--- jquery.valid.js
+++ jquery.valid.js
@@ -1,11 +1,24 @@
 /*
  * Valid - jQuery plugin
+ * jQuery plugin to validate forms
  * Author: Georges Duverger
  */
 
 // Methods attached to the jQuery.fn object
 
+// Validate one input or an entire form
+// ex.: $("input#example").valid()
+// ex.: $("form#example").vaild()
+//
+// With or without callbacks
+// ex.: $("input#example").valid({
+//	success: function(selector) { … },
+//	error: function(selector, error) { … },
+//	complete: function(selector, validity) { … }
+// })
+
 jQuery.fn.valid = function(options) {
+	// Declare the default callabacks
 	var defaults = {
 		success: function(selector) { $(selector).removeClass("invalid"); $(selector).addClass("valid") },
 		error: function(selector, error) { $(selector).removeClass("valid"); $(selector).addClass("invalid") },
@@ -16,8 +29,10 @@ jQuery.fn.valid = function(options) {
 	var validity = true;
 	this.each(function(){
 		// Mandatory double code to preserve the 'this' in the DOM
+		// If the current event as a data-valid attribute
 		if($(this).attr("data-valid")) {
 			try {
+				// Evaluate the method in the data-valid attribute
 				if(eval($(this).attr("data-valid"))) {
 					settings["success"](this);
 				} else {
@@ -28,8 +43,10 @@ jQuery.fn.valid = function(options) {
 				validity = false;
 			}
 		}
+		// Otherwise, we look for descendants with a data-valid attribute
 		$("[data-valid]", this).each(function() {
 			try {
+				// Evaluate the method in the data-valid attribute
 				if(eval($(this).attr("data-valid"))) {
 					settings["success"](this);
 				} else {
@@ -41,12 +58,11 @@ jQuery.fn.valid = function(options) {
 			}
 		})
 	});
-	
 	settings["complete"](this, validity);
 	return this;
 };
 
-// http://acmesoffware.com/acme/default.asp
+// Source: http://acmesoffware.com/acme/default.asp
 function isInteger(s) {
 	// console.log("s: "+s)
 	var i;
